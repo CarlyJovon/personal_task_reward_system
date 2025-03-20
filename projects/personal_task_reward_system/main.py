42fec19a07afb0f721cdefbffeeeb7a06e9733a6
Carly Johnson <carlyjohnson@SURFACE-D07.>
2025-03-20 07:40:10 -0400

    Initial commit - project outline

---
+++ projects/personal_task_reward_system/main.py
@@ -0,0 +1,17 @@
+# Personal Task Reward System 
+
+# 1. Ask for the user's name (input)
+
+# 2 .Present a list of daily tasks (minimum of 3)
+# Create a list to track completed tasks
+
+# 3. Allow use to mark task as completed (Loop through tasks)
+    # 3.1 Ask user if they have completed those tasks
+
+    # 3.2 Check if input is valid (answer = "yes" or "no")
+
+# 4. Calculate completion progress percentage based on # of tasks completed
+
+# 5. Provide encouraging feedback based on completion level (using if-else statements based on milestones)
+
+# 6. Print the results (user's name, completed tasks, progress, and feedback)
