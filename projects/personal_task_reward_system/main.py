0fcd6e6aefbb4c401a7e66dd506de14afcb263af
Carly Johnson <carlyjovon@gmail.com>
2025-03-20 12:48:23 -0400

    Added progress calculation and feedback based on task completion.

--- projects/personal_task_reward_system/main.py
+++ projects/personal_task_reward_system/main.py
@@ -1,13 +1,16 @@
 # Personal Task Reward System 
 
 # 1. Ask for the user's name (input)
-name = input("What is your name?")
+user_name = input("What is your name?")
 
 # 2 .Present a list of daily tasks (minimum of 3)
 # Create a list to track completed tasks
 tasks = ["Gratitude Journaling", "Vinyasa Yoga Class", "Coding Project"]
 
 # 3. Allow use to mark task as completed (Loop through tasks)
+# Create a list to track completed tasks
+completed_tasks = []
+
 for task in tasks: 
 
     # 3.1 Ask user if they have completed those tasks
@@ -21,7 +24,19 @@ for task in tasks:
         print("Invalid input. Please enter 'yes' or 'no'.")
 
 # 4. Calculate completion progress percentage based on # of tasks completed
-
+progress = (len(completed_tasks) / len(tasks)) * 100
 # 5. Provide encouraging feedback based on completion level (using if-else statements based on milestones)
-
+if progress == 100: 
+    print(f"Amazing job {user_name}! You've completed all of your tasks!")
+elif progress >= 75:
+    feedback = (f"Great job {user_name}! You're almost there!")
+elif progress >= 50:
+    feedback = (f"Keep going {user_name}! You're more than halfway there!")
+elif progress >= 25:
+    feedback = (f"Keep going {user_name}! You can do it!")
+else:
+    feedback = "Don't give up! Every little bit helps."
 # 6. Print the results (user's name, completed tasks, progress, and feedback)
+print(f"{user_name}, you completed {len(completed_tasks)} out of {len(tasks)} tasks.")
+print(f"Progress: {progress:.2f}%")
+print(feedback)
